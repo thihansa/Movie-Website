b66a8edd54f2c073054179b2c463c71f21e9a9be
Thihansa Mudalige <61136235+thihansa@users.noreply.github.com>
2023-03-27 18:53:00 +0530

    Add files via upload

---
+++ Student_1/Selecting_javascript.js
@@ -0,0 +1,51 @@
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
+
+function joker() {
+  location.replace("Sale/JokerProducts.html")
+}
+
+function darbar() {
+  location.replace("Sale/DarbarProducts.html")
+}
+
+function one() {
+  location.replace("Sale/1917Products.html")
+}
+
+function hero() {
+  location.replace("Sale/HeroProducts.html")
+}
+
+function zombie() {
+  location.replace("Sale/ZombielandProducts.html")
+}
+
+function shazam() {
+  location.replace("Sale/ShazamProducts.html")
+}
+
+function birds() {
+  location.replace("Sale/AngryBirds2Products.html")
+}
+
+function toy() {
+  location.replace("Sale/ToyStory4Products.html")
+}
+
+function vedha() {
+  location.replace("Sale/VikramVedhaProducts.html")
+}
+
+function it() {
+  location.replace("Sale/IT2Products.html")
+}
+
+function kaithi() {
+  location.replace("Sale/KaithiProducts.html")
+}
+
+function quiet() {
+  location.replace("Sale/QuietPlaceProducts.html")
+}
